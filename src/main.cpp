f58cd5419699d4e1c2c880980b1d1bc0b92e2943
Kris <jiri.vimmer123@seznam.cz>
2024-06-09 15:33:10 +0200

    Modified the code to use a joystick

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@
 
 #define RED_LED D5
 #define GREEN_LED D6
+#define JOYSTICK_PIN A0
 
 Adafruit_MPU6050 mpu;
 
-/* Prints sensor data to the serial output */
+/* Prints data to the serial output */
 void print_data(void);
 
 /* Initializes peripherials and serial */
@@ -21,7 +22,7 @@ void init_devices(void){
   digitalWrite(GREEN_LED, LOW);
 
   Serial.begin(9600);
-  while(!mpu.begin()){
+  while(!mpu.begin()){ // Checks if the sensor is connected and working
     Serial.println("SENSOR ERROR!");
     digitalWrite(RED_LED, HIGH);
     delay(500);
@@ -30,8 +31,9 @@ void init_devices(void){
   digitalWrite(RED_LED, LOW);
   digitalWrite(GREEN_LED, HIGH);
 
-  // Disable cycling and sleeping
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  
+  // Disable cycling and sleeping
   mpu.enableCycle(false);
   mpu.enableSleep(false);
 }
@@ -45,6 +47,8 @@ void loop(void){
 }
 
 void print_data(void){
+  float joystickData = analogRead(JOYSTICK_PIN);
+
   sensors_event_t accel, gyro, temp;
   mpu.getEvent(&accel, &gyro, &temp);
 
@@ -53,7 +57,10 @@ void print_data(void){
   Serial.print(accel.gyro.y);
   Serial.print(";"); 
   Serial.print(accel.gyro.z);
+  Serial.print(";");
+  Serial.print(joystickData);
   Serial.println();
 
-  delay(50);
+  //delay(50);
+  delay(20);
 }
